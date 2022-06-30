aa1bdde87234944c3d28185472f17b620de7cda6
Sean Oh <seanoh@ucsb.edu>
2022-06-30 11:12:26 -0700

    add heading and current time

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var currentTimeEl = $("#currentTime");
+
+function displayCurrTime() {
+  var timeInterval = setInterval(function() {
+    var currTime = moment().format("MMM Do, YYYY hh:mm:ssa");
+    console.log(currTime);
+    currentTimeEl.text(currTime);
+  },1000);
+}
+
+displayCurrTime();
